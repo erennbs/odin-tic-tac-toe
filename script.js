6ad51b3994b2ec528af7caeab96f4a9780492481
Eren Bas <erenbas3453@gmail.com>
2024-02-18 19:13:06 +0300

    Rewrite constructers and factory functions with class

--- script.js
+++ script.js
@@ -1,178 +1,199 @@
-function Player(name, mark) {
-    this.name = name
-    this.mark = mark,
-    this.score = 0
-}
+class Player {
 
-function Gameboard() {
-    this.board = [[null, null, null], 
-                  [null, null, null], 
-                  [null, null, null]]    
-}
-
-Gameboard.prototype.PrintBoard = function(){
-    for (let i = 0; i < 3; i++) {
-        console.log(this.board[i][0] + " | " + this.board[i][1] + " | " + this.board[i][2] + "\n");
+    constructor(name, mark) {
+        this.name = name
+        this.mark = mark,
+        this.score = 0
     }
 }
 
-Gameboard.prototype.ResetBoard = function(){
-    for(let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            this.board[i][j] = null;
+class Gameboard {
+    constructor() {
+        this.board = [[null, null, null], 
+                      [null, null, null], 
+                      [null, null, null]]    
+    }
+
+    PrintBoard() {
+        for (let i = 0; i < 3; i++) {
+            console.log(this.board[i][0] + " | " + this.board[i][1] + " | " + this.board[i][2] + "\n");
+        }
+    }
+
+    ResetBoard() {
+        for(let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                this.board[i][j] = null;
+            }
         }
     }
 }
 
-function GameController(player1, player2) {
-    const _player1 = player1;
-    const _player2 = player2;
-
-    let isFinished = false;
-
-    const board = new Gameboard();
-
-    let activePlayer = _player1;
-
-    const IsFinished = () => {
-        return isFinished;
-    }
-
-    const ChangeActivePlayer = () => {
-        activePlayer = activePlayer === _player1 ? _player2 : _player1;
-    }
-
-    const GetActivePlayer = () => {
-        return activePlayer;
-    }
-
-    const CheckIfPlacable = (row, col) => {
-        return board.board[row][col] === null;
-    }
-
-    const PlayRound = (row, col) => {
-        if (!CheckIfPlacable(row, col)) return;
-
-        board.board[row][col] = activePlayer.mark;
+class GameController {
+    #player1;
+    #player2;
+    #activePlayer;
+    
+    #isFinished = false;
+    #board = new Gameboard();
+    
+    constructor(player1, player2) {
+        this.#player1 = player1;
+        this.#player2 = player2;
         
-        if (CheckWin(row, col)) {
-            FinishGame(activePlayer);
+        this.#activePlayer = this.#player1;
+    }
+
+    IsFinished() {
+        return this.#isFinished;
+    }
+
+    #ChangeActivePlayer() {
+        this.#activePlayer = this.#activePlayer === this.#player1 ? this.#player2 : this.#player1;
+    }
+
+    GetActivePlayer() {
+        return this.#activePlayer;
+    }
+
+    CheckIfPlacable(row, col) {
+        return this.#board.board[row][col] === null;
+    }
+
+    PlayRound(row, col) {
+        if (!this.CheckIfPlacable(row, col)) return;
+
+        this.#board.board[row][col] = this.#activePlayer.mark;
+        
+        if (this.#CheckWin(row, col)) {
+            this.#FinishGame(this.#activePlayer);
         } else {
-            ChangeActivePlayer();
+            this.#ChangeActivePlayer();
         }
         
     }
 
-    const FinishGame = () => {
-        isFinished = true;
-        activePlayer.score += 1;
-        console.log(`Winner is ${activePlayer.name}!`);
+    #FinishGame() {
+        this.#isFinished = true;
+        this.#activePlayer.score += 1;
+        console.log(`Winner is ${this.#activePlayer.name}!`);
     }
 
-    const RestartGame = () => {
-        isFinished = false;
-        board.ResetBoard();
-        activePlayer = _player1;
+    RestartGame() {
+        this.#isFinished = false;
+        this.#board.ResetBoard();
+        this.#activePlayer = this.#player1;
     }
 
-    const GetPlayers = () => {
-        return [_player1, _player2];
+    GetPlayers() {
+        return [this.#player1, this.#player2];
     }
 
-    const CheckWin = (row, col) => {
-        if (checkHorizontal(row) || checkVertical(col) || checkDiagonal())  
+    #CheckWin(row, col) {
+        if (this.#CheckHorizontal(row) || this.#CheckVertical(col) || this.#CheckDiagonal())  
         return true;
         else return false;
     }
 
-    const checkHorizontal = (row) => {
+    #CheckHorizontal(row) {
         for(let i = 0; i < 3; i++) {
-            if(board.board[row][i] !== activePlayer.mark) return false;
+            if(this.#board.board[row][i] !== this.#activePlayer.mark) return false;
         }
         return true
     }
 
-    const checkVertical = (col) => {
+    #CheckVertical(col) {
         for (let i = 0; i < 3; i++) {
-            if (board.board[i][col] !== activePlayer.mark) return false;
+            if (this.#board.board[i][col] !== this.#activePlayer.mark) return false;
         }
         return true
     }
 
-    const checkDiagonal = () => {
+    #CheckDiagonal() {
         let d1 = true
         let d2 = true
         
         for(let i = 0; i < 3; i++) {
-            if(board.board[i][i] !== activePlayer.mark) d1 = false;
+            if(this.#board.board[i][i] !== this.#activePlayer.mark) d1 = false;
         }
         
         for(let i = 2; i >= 0; i--) {
-            if(board.board[2 - i][i] !== activePlayer.mark) d2 = false;
+            if(this.#board.board[2 - i][i] !== this.#activePlayer.mark) d2 = false;
         }
 
         return d1 || d2;
     }
-
-    return {PlayRound, IsFinished, GetActivePlayer, CheckIfPlacable, RestartGame, GetPlayers}
 }
 
-function DOMController(gameController) {
-    const _gameController = gameController;
-
-    const p1Name = document.querySelector(".p1-name");
-    const p1Score = document.querySelector(".p1-score");
+class DOMController {
+    #gameController;
     
-    const p2Name = document.querySelector(".p2-name");
-    const p2Score = document.querySelector(".p2-score");
+    #p1Name;
+    #p1Score;
     
-    const restartButton = document.querySelector(".restart-button");
-    const cells = document.querySelectorAll(".cells button");
+    #p2Name
+    #p2Score;
 
-    p1Name.textContent = _gameController.GetPlayers()[0].name;
-    p1Score.textContent = _gameController.GetPlayers()[0].score;
+    #restartButton
+    #cells;
 
-    p2Name.textContent = _gameController.GetPlayers()[1].name;
-    p2Score.textContent = _gameController.GetPlayers()[1].score;
+    constructor(gameController) {
+        this.#gameController = gameController;
+
+        this.#p1Name = document.querySelector(".p1-name");
+        this.#p1Score = document.querySelector(".p1-score");
+        
+        this.#p2Name = document.querySelector(".p2-name");
+        this.#p2Score = document.querySelector(".p2-score");
+        
+        this.#restartButton = document.querySelector(".restart-button");
+        this.#cells = document.querySelectorAll(".cells button");
     
-    restartButton.addEventListener('click', (event) => {
-        _gameController.RestartGame();
-        ClearCells();
-    })
+        this.#p1Name.textContent = this.#gameController.GetPlayers()[0].name;
+        this.#p1Score.textContent = this.#gameController.GetPlayers()[0].score;
+    
+        this.#p2Name.textContent = this.#gameController.GetPlayers()[1].name;
+        this.#p2Score.textContent = this.#gameController.GetPlayers()[1].score;
+        
+        this.#restartButton.addEventListener('click', (event) => {
+            this.#gameController.RestartGame();
+            this.#ClearCells();
+        });
+    
+        this.#cells.forEach(cell => {
+            cell.addEventListener('click', (event) => {
+                if (this.#gameController.IsFinished()) return;
+    
+                let target = event.target;
+                let row = Number(target.dataset.row)
+                let col = Number(target.dataset.col)
+                if (!this.#gameController.CheckIfPlacable(row, col)) return;
+    
+                cell.textContent = this.#gameController.GetActivePlayer().mark;
+                cell.style.color = this.#gameController.GetActivePlayer().mark == "X" ? "salmon" : "lightskyblue";
+                this.#gameController.PlayRound(row, col);
+    
+                if (this.#gameController.IsFinished()) this.#UpdateScores();
+            })
+        });
+    }
 
-    cells.forEach(cell => {
-        cell.addEventListener('click', (event) => {
-            if (_gameController.IsFinished()) return;
-
-            let target = event.target;
-            let row = Number(target.dataset.row)
-            let col = Number(target.dataset.col)
-            if (!gameController.CheckIfPlacable(row, col)) return;
-
-            cell.textContent = _gameController.GetActivePlayer().mark;
-            cell.style.color = _gameController.GetActivePlayer().mark == "X" ? "salmon" : "lightskyblue";
-            _gameController.PlayRound(row, col);
-
-            if (_gameController.IsFinished()) UpdateScores();
-        })
-    });
-
-    const ClearCells = () => {
-        cells.forEach(cell => {
+    #ClearCells() {
+        this.#cells.forEach(cell => {
             cell.textContent = "";
         })};
 
-    const UpdateScores = () => {
-        p1Score.textContent = _gameController.GetPlayers()[0].score;
-        p2Score.textContent = _gameController.GetPlayers()[1].score;
+    #UpdateScores() {
+        this.#p1Score.textContent = this.#gameController.GetPlayers()[0].score;
+        this.#p2Score.textContent = this.#gameController.GetPlayers()[1].score;
     }
 }
 
 player1 = new Player("P1", "X");
 player2 = new Player("P2", "O");
 
-let game = GameController(player1, player2);
-let dom = DOMController(game);
+let game = new GameController(player1, player2);
+let dom = new DOMController(game);
 
 // while (!game.IsFinished()) {
 //     input = prompt("Row,Column: ").split(",")
